9698c5cc2761902a35547d253085a3862e79ade8
Elliop <abdelmouhayminegx@gmail.com>
2021-10-27 19:33:04 +0100

    Building the Feed Component

---
+++ components/Feed.jsx
@@ -0,0 +1,16 @@
+const Feed = () => {
+  return (
+    <div>
+      <section>
+        {/* Stories */}
+        {/* Posts */}
+      </section>
+      <section>
+        {/* Mini Profile */}
+        {/* Suggestions */}
+      </section>
+    </div>
+  );
+};
+
+export default Feed;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Feed from "../components/Feed";
 import Header from "../components/Header";
 
 export default function Home() {
@@ -9,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      {/* Header */}
+      <Feed />
       {/* Feed */}
       {/* Model */}
     </div>
