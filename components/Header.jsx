cb78a9d36d6c5eca5ac9421db106883246e50b47
Elliop <abdelmouhayminegx@gmail.com>
2021-10-27 14:20:08 +0100

    Add logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,12 @@
+import Image from "next/image";
+
 const Header = () => {
   return (
     <div>
       {/* Left */}
-
+      <div>
+        <Image src="/Logo.png" width={120} height={40} layout="fixed" />
+      </div>
       {/* Middle */}
 
       {/* Right */}
