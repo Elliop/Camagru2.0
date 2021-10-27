fde61bced23f3b9108943e9f9ece97abd5029304
Elliop <abdelmouhayminegx@gmail.com>
2021-10-27 14:45:14 +0100

    Building the Left Section of the Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,13 +3,18 @@ import Image from "next/image";
 const Header = () => {
   return (
     <div>
-      {/* Left */}
-      <div>
-        <Image src="/Logo.png" width={120} height={40} layout="fixed" />
-      </div>
-      {/* Middle */}
+      <div className="flex justify-between bg-red max-w-6xl">
+        {/* Left */}
+        <div className="relative hidden lg:inline-grid w-24 h-10 cursor-pointer">
+          <Image src="/Logo.png" layout="fill" objectFit="contain" />
+        </div>
+        <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+          <Image src="/C.png" layout="fill" objectFit="contain" />
+        </div>
+        {/* Middle */}
 
-      {/* Right */}
+        {/* Right */}
+      </div>
     </div>
   );
 };
