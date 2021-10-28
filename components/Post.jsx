8bb33fcdc2f8936bf96d45737f77b7796665fd1d
Elliop <abdelmouhayminegx@gmail.com>
2021-10-28 17:45:18 +0100

    Building the Header Section of the Post

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,14 +1,29 @@
+import {
+  BookmarkIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  EmojiHappyIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+} from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+
 const Post = ({ id, username, userImg, img, caption }) => {
   return (
-    <div>
+    <div className="bg-white my-7 border rounded-sm">
       {/* Header */}
-      <div>
-        <img src={userImg} alt="Profile pic" />
-        <p>{username}</p>
+      <div className="flex items-center p-5">
+        <img
+          src={userImg}
+          alt="Profile pic"
+          className="w-12 h-12 rounded-full object-contain border p-1 mr-3"
+        />
+        <p className="flex-1 font-bold">{username}</p>
+        <DotsHorizontalIcon className="h-5 cursor-pointer" />
       </div>
 
       {/* img */}
-      <img src={img} />
+      <img src={img} className="object-cover w-full" />
 
       {/* Buttons */}
 
